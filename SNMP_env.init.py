d3d602239b9b3bf00a9dda460cefeb592d40ea3a
Stephen O'Kennedy <stephenoken@gmail.com>
2016-03-25 10:09:49 +0000

    "Added python config docs"
    ""

---
+++ SNMP_env.init.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
+
+    info( '*** Add hosts\n')
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(s1, h4)
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(s1, h3)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s1').start([])
+
+    info( '*** Post configure switches and hosts\n')
+    hosts = [h1, h2, h3, h4]
+    printInfo(hosts)
+    CLI(net)
+    net.stop()
+
+def printInfo(hosts):
+    for h in hosts:
+        print "Host", h.name, "has IP address", h.IP()
+    print "Run agent on host h1, master can be run anywhere"
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
