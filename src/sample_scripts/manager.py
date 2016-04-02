5b44fb4deaf8162ff421f8967a60e1ce2f48d17d
Stephen O'Kennedy <stephenoken@gmail.com>
2016-04-02 14:20:06 +0100

    "Changed the ip addresses in the manager script"
    ""

--- src/sample_scripts/manager.py
+++ src/sample_scripts/manager.py
@@ -33,7 +33,7 @@ def pollFn():
     errorIndication, errorStatus, errorIndex, varBinds = next(
         getCmd(SnmpEngine(),
             CommunityData('public',mpModel=0),
-            UdpTransportTarget(('localhost', 1162)),
+            UdpTransportTarget(('10.0.0.1', 1161)),
             ContextData(),
             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')))
     )
@@ -54,7 +54,7 @@ def pollFn():
     errorIndication, errorStatus, errorIndex, varBinds = next(
         getCmd(SnmpEngine(),
             CommunityData('public',mpModel=0),
-            UdpTransportTarget(('localhost', 1161)),
+            UdpTransportTarget(('10.0.0.2', 1161)),
             ContextData(),
             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')))
     )
